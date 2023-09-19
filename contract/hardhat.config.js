b4a0a1c7d4ea84c788febb09d13e996e12c84418
Hexdee <lukmansaheed79@gmail.com>
2023-09-19 11:52:45 +0100

    Add pegochain config

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -4,4 +4,17 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    'pego': {
+      url: 'https://pegorpc.com/',
+      accounts: [process.env.WALLET_KEY],
+      gasPrice: 1000000000,
+    },
+    // for testnet
+    'pego-testnet': {
+      url: 'https://rpc.pegotest.net/',
+      accounts: [process.env.WALLET_KEY],
+      gasPrice: 1000000000,
+    },
+  },
 };
